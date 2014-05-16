ab07d06632c618fdd4d7d8454d14b0485082b98c
Thibault BRONCHAIN <thibault@mc2.io>
2014-05-16 15:26:30 +0800

    actors tests

--- hello-akka/src/main/scala/hello.scala
+++ hello-akka/src/main/scala/hello.scala
@@ -11,13 +11,17 @@ class HelloActor extends Actor {
   def receive = {
     case HelloActor.SayHello(message) =>
       println(message)
+      Thread.sleep(1000)
   }
 }
 
 object Main extends App {
   val system = ActorSystem("ActorSystem")
 
-  val hello = system.actorOf(Props[HelloActor])
+  val hello1 = system.actorOf(Props[HelloActor], "hello1")
+  val hello2 = system.actorOf(Props[HelloActor], "hello2")
 
-  hello ! HelloActor.SayHello("Hello!")
+  hello1 ! HelloActor.SayHello("Hello!")
+  hello1 ! HelloActor.SayHello("Hello World1!")
+  hello2 ! HelloActor.SayHello("Hello World2!")
 }
